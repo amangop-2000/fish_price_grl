3d81cba2209ddd35f9937c0cc90cdb50d52e4ece
amangop-2000 <amangopkv@gmail.com>
2025-10-20 14:42:43 +0530

    changes for render

--- backend/index.js
+++ backend/index.js
@@ -96,4 +96,8 @@ app.get("/fishes/updated_today", async (req, res) => {
 // ---------------- START SERVER ---------------- //
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
+
